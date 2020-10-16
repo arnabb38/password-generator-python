51c22c6e586ba54d7264aefb3258bf6ff8c3435e
arnabb38 <arnabbasak896@gmail.com>
2020-10-17 00:53:59 +0600

    update script

--- password-generator.py
+++ password-generator.py
@@ -5,10 +5,13 @@ smallletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".lower()
 symbols = "!@#$%*-_"
 numeric = "0123456789"
 
+#combining character set
 stringcombination = capletters + smallletters + symbols + numeric
 
+#length of the password
 length =  12
 
+#generating random password
 generatedpassword = "".join(random.sample(stringcombination, length))
 
 print(generatedpassword)
